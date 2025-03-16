b249fb67e34a5ddf109ef3a5b640e87d7aa99bf7
nilsmuralles <murallesnils11@gmail.com>
2025-03-15 18:45:11 -0600

    feat: Style added to messages

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ const messages = [
     "text": "Este es un mensaje mio",
     "user": "Nils"
   },
+  {
+    "id": 5,
+    "text": "Este mensaje asjdfksdjflkdasjf lkje es de alguien más después de uno mío",
+    "user": "Diego"
+  },
 ]
 
 // Código final
@@ -41,11 +46,13 @@ const renderHomePage = () => {
 
   main.style.fontFamily = font
   main.style.color = "White"
-  main.style.textAlign = "center"
   main.style.padding = "10px"
 
   h1.append('Web - Sección 20')
   h1.style.fontWeight = "lighter"
+  h1.style.textAlign = "center"
+
+  messagesSection.style.padding = '20px'
 
   main.append(h1)
   main.append(messagesSection)
@@ -56,12 +63,41 @@ const renderHomePage = () => {
 
 const renderMessages = () => {
   const messagesSection = document.querySelector('.messages-section')
+
   const messagesList = document.createElement('ul')
+  messagesList.style.listStyle = 'none'
+  messagesList.style.margin = '0'
+  messagesList.style.padding = '0'
+  messagesList.style.display = 'flex'
+  messagesList.style.flexDirection = 'column'
+  messagesList.style.gap = '20px'
 
   messages.forEach(message => {
-    const messageLi = document.createElement('li')
-    messageLi.textContent = message.user + " " + message.text
-    messagesList.appendChild(messageLi)
+    const li = document.createElement('li')
+    li.style.maxWidth = 'fit-content'
+    li.style.display = 'flex'
+    li.style.flexDirection = 'column'
+    li.style.gap = '5px'
+    li.style.padding = '15px 20px'
+    li.style.background = '#403F44'
+    li.style.borderRadius = '8px'
+
+    const userSpan = document.createElement('span')
+    userSpan.textContent = message.user
+    userSpan.style.fontWeight = 'bold'
+
+    const messageP = document.createElement('p')
+    messageP.textContent = message.text
+    messageP.style.margin = '0'
+
+    if (message.user === 'Nils') {
+      li.style.alignSelf = 'flex-end'
+      li.style.background = '#404156'
+    }
+
+    li.append(userSpan)
+    li.append(messageP)
+    messagesList.appendChild(li)
   })
   
   messagesSection.appendChild(messagesList)
